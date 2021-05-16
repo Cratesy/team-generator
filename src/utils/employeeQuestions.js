87f8b07a8690cdcead035344b29250db4d4177f5
Michael Crates <michaelcrates89@hotmail.co.uk>
2021-05-16 15:19:55 +0100

    created employee questions

---
+++ src/utils/employeeQuestions.js
@@ -0,0 +1,19 @@
+const employeeQuestions = [
+  {
+    type: "input",
+    message: "Please enter the employees name?",
+    name: "name",
+  },
+  {
+    type: "number",
+    message: "What is the employees ID number?",
+    name: "id",
+  },
+  {
+    type: "input",
+    message: "What is the employees email address?",
+    name: "email",
+  },
+];
+
+module.exports = employeeQuestions;

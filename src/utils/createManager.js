0da796dfa16d92df36a1dc9a63dd81be68230fb1
Michael Crates <michaelcrates89@hotmail.co.uk>
2021-05-16 15:23:03 +0100

    manager questions for creating a manager

---
+++ src/utils/createManager.js
@@ -0,0 +1,22 @@
+const Manager = require("../../lib/Manager");
+const employeeQuestions = require("./employeeQuestions");
+const getAnswers = require("./getAnswers");
+
+const createManager = async () => {
+  const questions = [
+    ...employeeQuestions,
+    {
+      type: "input",
+      message: "What is the managers office number?",
+      name: "officeNumber",
+    },
+  ];
+
+  const answers = await getAnswers(questions);
+
+  const manager = new Manager(answers);
+
+  return manager;
+};
+
+module.exports = createManager;
